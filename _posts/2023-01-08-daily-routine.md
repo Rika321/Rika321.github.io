a4cb180c66a88c68801d6e09fe1cf2b4518a890b
He Qu <he.qu@oracle.com>
2024-01-09 00:20:31 -0800

    new post daily routine

---
+++ _posts/2023-01-08-daily-routine.md
@@ -0,0 +1,31 @@
+---
+title:  "Daily routine"
+search: true
+categories: 
+  - Biohack
+tags:
+  - Biohack
+  - Self
+last_modified_at: 2024-01-08
+---
+
+I am still experimenting a good personal daily routine
+
+## Morning routine:
+I used to neglect the importance of the morning hours. To live a heathier life, I summarized a simplified and practical morning routine as followings:
+
+  1. Dont scroll the phone until the routine ends
+  2. Oral cleasing with floss/toothbrush
+  3. (Optional) Cold shower
+  4. Smoothie and breakfast preparation
+  5. LLLR therapy and meditation for 4 minutes
+  6. Take a walk outside (bonus point to practice basketball)
+   
+
+## Winddown routine:
+I want to improve my sleep quality with the following routines:
+
+  1. Supplements
+  2. Oral cleasing with floss/toothbrush
+  3. Journaling
+  4. Don't scroll the phone on the bed
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,6 +8,12 @@ var store = [{
         "title": "Rika's blog is online",
         "excerpt":"Rika’s primary objective to blog. In 2023, I was inspired by some longevity influencers (Bryan Johnson, Petter Attia, David Sinclair, Rhonda Patrick, Rrad Stanfield etc) and started to play around with my bodily system and brain. I want to log my exploration progress on my own blog. I plan to...","categories": ["Misc"],
         "tags": ["Biohack","Misc"],
-        "url": "/misc/first-post/",
+        "url": "/misc/first-post-copy/",
+        "teaser": null
+      },{
+        "title": "Daily routine",
+        "excerpt":"I am still experimenting a good personal daily routine Morning routine: I used to neglect the importance of the morning hours. To live a heathier life, I summarized a simplified and practical morning routine as followings: Dont scroll the phone until the routine ends Oral cleasing with floss/toothbrush (Optional) Cold...","categories": ["Biohack"],
+        "tags": ["Biohack","Self"],
+        "url": "/biohack/daily-routine/",
         "teaser": null
       }]
